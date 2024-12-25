54aa23522042124f001b34e415c9978e7d34e755
matheus <matheus.mmn@hotmail.com>
2024-12-25 02:06:49 -0300

    ajustes de path do css e js

--- assets/php/partials/menu.php
+++ assets/php/partials/menu.php
@@ -1,23 +1,23 @@
 <li class="nav-item active">
-    <a href="./index.html" class="collapsed">
+    <a href="/lumex/index.php" class="collapsed">
         <i class="fas fa-home"></i>
         <p>Dashboard</p>
     </a>
 </li>
 <li class="nav-item">
-    <a href="./forms/nova_proposta.php">
+    <a href="/lumex/forms/nova_proposta.php">
         <i class="fas fa-plus"></i>
         <p>Nova Proposta</p>
     </a>
 </li>
 <li class="nav-item">
-    <a href="./tables/propostas.html">
+    <a href="/lumex/tables/propostas.html">
         <i class="fas fa-file"></i>
         <p>Propostas</p>
     </a>
 </li>
 <li class="nav-item">
-    <a href="../forms/empresa.html">
+    <a href="/lumex/forms/empresa.html">
         <i class="fas fa-file"></i>
         <p>Minha empresa</p>
     </a>
