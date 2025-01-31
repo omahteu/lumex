bf8b55993780ad39dabbab4ea024c9f523e826c2
matheus <matheus.mmn@hotmail.com>
2025-01-31 13:23:40 -0300

    avanço na criação das propostas

--- forms/nova_proposta.php
+++ forms/nova_proposta.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <?php include("../assets/php/partials/header.php"); ?>
+  <?php include "../assets/php/partials/header.php"; ?>
   <body>
     <div class="wrapper">
       <!-- Sidebar -->
@@ -8,7 +8,7 @@
         <div class="sidebar-logo">
           <!-- Logo Header -->
           <div class="logo-header" data-background-color="dark">
-            <?php include('../assets/php/partials/logo.php') ?>
+            <?php include '../assets/php/partials/logo.php'?>
             <div class="nav-toggle">
               <button class="btn btn-toggle toggle-sidebar">
                 <i class="gg-menu-right"></i>
@@ -26,14 +26,14 @@
         <div class="sidebar-wrapper scrollbar scrollbar-inner">
           <div class="sidebar-content">
             <ul class="nav nav-secondary">
-              <?php include('../assets/php/partials/menu.php') ?>
+              <?php include '../assets/php/partials/menu.php'?>
               <li class="nav-item">
                 <a data-bs-toggle="collapse" href="#submenu">
                   <i class="fas fa-bars"></i>
                   <p>Dados</p>
                   <span class="caret"></span>
                 </a>
-                <?php include('../assets/php/partials/submenu.php') ?>
+                <?php include '../assets/php/partials/submenu.php'?>
               </li>
             </ul>
           </div>
@@ -46,7 +46,7 @@
           <div class="main-header-logo">
             <!-- Logo Header -->
             <div class="logo-header" data-background-color="dark">
-              <?php include('./assets/php/partials/logo.php') ?>
+              <?php include './assets/php/partials/logo.php'?>
               <div class="nav-toggle">
                 <button class="btn btn-toggle toggle-sidebar">
                   <i class="gg-menu-right"></i>
@@ -92,21 +92,21 @@
                         <h2>Cliente</h2>
                         <div class="row">
                           <?php
-                            require_once '../assets/php/crud.php';
-                            $db = new Database();
-                            $queryCliente = "SELECT id, nome, distribuidora, descricao_unidade_consumidora, (mes_1 + mes_2 + mes_3 + mes_4 + mes_5 + mes_6 + mes_7 + mes_8 + mes_9 + mes_10 + mes_11 + mes_12) / 12 AS media_consumo FROM clientes";
-                            $clientes = json_decode($db->executeQuery($queryCliente), true);
-                            $queryIrradiacao = "SELECT id, cidade, irradiacao FROM irradiacoes";
-                            $irradiacoes = json_decode($db->executeQuery($queryIrradiacao), true);
+                              require_once '../assets/php/crud.php';
+                              $db              = new Database();
+                              $queryCliente    = "SELECT id, nome, distribuidora, descricao_unidade_consumidora, (mes_1 + mes_2 + mes_3 + mes_4 + mes_5 + mes_6 + mes_7 + mes_8 + mes_9 + mes_10 + mes_11 + mes_12) / 12 AS media_consumo FROM clientes";
+                              $clientes        = json_decode($db->executeQuery($queryCliente), true);
+                              $queryIrradiacao = "SELECT id, cidade, irradiacao FROM irradiacoes";
+                              $irradiacoes     = json_decode($db->executeQuery($queryIrradiacao), true);
                           ?>
                           <div class="col-md6 col-6">
                             <div class="form-group">
                             <select name="cliente" id="cliente" class="form-control" onchange="preencherTabela()">
                               <option value="" hidden>Cliente</option>
                               <?php
-                                foreach ($clientes as $cliente) {
-                                  echo "<option value='" . $cliente['id'] . "' data-nome='" . $cliente['nome'] . "' data-media='" . $cliente['media_consumo'] . "' data-distribuidora='" . $cliente['distribuidora'] . "' data-descricao='" . $cliente['descricao_unidade_consumidora'] . "'>" . $cliente['nome'] . "</option>";
-                                }
+                                  foreach ($clientes as $cliente) {
+                                      echo "<option value='" . $cliente['id'] . "' data-nome='" . $cliente['nome'] . "' data-media='" . $cliente['media_consumo'] . "' data-distribuidora='" . $cliente['distribuidora'] . "' data-descricao='" . $cliente['descricao_unidade_consumidora'] . "'>" . $cliente['nome'] . "</option>";
+                                  }
                               ?>
                             </select>
                             </div>
@@ -116,9 +116,9 @@
                             <select name="irradiacao" id="irradiacao" class="form-control" onchange="preencherIrradiacao()">
                               <option value="" hidden>Irradiação</option>
                               <?php
-                                foreach ($irradiacoes as $irradiacao) {
-                                  echo "<option value='" . $irradiacao['id'] . "' data-irradiacao='" . $irradiacao['irradiacao'] . "'>" . $irradiacao['cidade'] . " | " . $irradiacao['irradiacao'] . "</option>";
-                                }
+                                  foreach ($irradiacoes as $irradiacao) {
+                                      echo "<option value='" . $irradiacao['id'] . "' data-irradiacao='" . $irradiacao['irradiacao'] . "'>" . $irradiacao['cidade'] . " | " . $irradiacao['irradiacao'] . "</option>";
+                                  }
                               ?>
                             </select>
                             </div>
@@ -156,22 +156,22 @@
 
                         <h2>Cálculo de Kit</h2>
                         <div class="row">
-                          <div class="col-md6 col-3">
+                          <div class="col-md-3 col-3">
                             <div class="form-group">
-                              <input type="number" class="form-control" id="email2" value="30" max="31" min="1"/>
-                              <small id="emailHelp2" class="form-text text-muted">Quantidade de dias</small>
+                              <input type="number" class="form-control" name="quantidade_dias" id="quantidade_dias" value="30" max="31" min="1"/>
+                              <small id="quantidade_dias_help" class="form-text text-muted">Quantidade de dias</small>
                             </div>
                           </div>
-                          <div class="col-md6 col-3">
+                          <div class="col-md-3 col-3">
                             <div class="form-group">
-                              <input type="number" class="form-control" id="email2" min="1" max="100" placeholder="Eficiência do sistema"/>
-                              <small id="emailHelp2" class="form-text text-muted">Eficiência do Sistema (%)</small>
+                              <input type="number" class="form-control" name="eficiencia" id="eficiencia" min="1" max="100" placeholder="Eficiência do sistema"/>
+                              <small id="eficiencia_help" class="form-text text-muted">Eficiência do Sistema (%)</small>
                             </div>
                           </div>
-                          <div class="col-md6 col-6">
+                          <div class="col-md-6 col-6">
                             <div class="form-group">
-                              <input type="text" class="form-control" id="email2" value="0" disabled/>
-                              <small id="emailHelp2" class="form-text text-muted">Potência do kit (Kwp)</small>
+                              <input type="text" class="form-control" name="potencia_kit" id="potencia_kit" value="0" disabled/>
+                              <small id="potencia_kit_help" class="form-text text-muted">Potência do kit (Kwp)</small>
                             </div>
                           </div>
                         </div>
@@ -182,37 +182,36 @@
                         <div class="row">
                           <div class="col-md6 col-2">
                             <div class="form-group">
-                              <select name="" id="" class="form-control">
+                              <select name="painel" id="painel" class="form-control">
                                 <option value="" hidden>Painel</option>
-                                <option value="">550</option>
-                                <option value="">570</option>
-                                <option value="">590</option>
-                                <option value="">690</option>
+                                <option value="550">550</option>
+                                <option value="570">570</option>
+                                <option value="590">590</option>
+                                <option value="690">690</option>
                               </select>
-                              <small id="emailHelp2" class="form-text text-muted">(Watts)</small>
-                            </div>
-
-                          </div>
-                          <div class="col-md6 col-3">
-                            <div class="form-group">
-                              <input type="text" class="form-control" id="email2" placeholder="Potência do painel" disabled/>
-                              <small id="emailHelp2" class="form-text text-muted">(Kwp)</small>
+                              <small id="painel_help2" class="form-text text-muted">(Watts)</small>
                             </div>
                           </div>
                           <div class="col-md6 col-3">
                             <div class="form-group">
-                              <input type="text" class="form-control" id="email2" placeholder="Quantidade de painéis" disabled/>
-                              <small id="emailHelp2" class="form-text text-muted"></small>
+                              <input type="text" class="form-control" name="potencia_painel" id="potencia_painel" placeholder="Potência do painel" disabled/>
+                              <small id="potencia_painel_help2" class="form-text text-muted">(Kwp)</small>
                             </div>
                           </div>
                           <div class="col-md6 col-3">
                             <div class="form-group">
-                              <select class="form-control" name="" id="">
+                              <input type="text" class="form-control" name="quantidade_painel" id="quantidade_painel" placeholder="Quantidade de painéis" disabled/>
+                              <small id="quantidade_painel_help2" class="form-text text-muted"></small>
+                            </div>
+                          </div>
+                          <div class="col-md6 col-3">
+                            <div class="form-group">
+                              <select class="form-control" name="estrutura" id="estrutura">
                                 <option value="" hidden>Tipo de Estrutura</option>
-                                <option value="">Colonial</option>
-                                <option value="">Fribocimento</option>
-                                <option value="">Metálico</option>
-                                <option value="">Solo</option>
+                                <option value="1">Colonial</option>
+                                <option value="2">Fribocimento</option>
+                                <option value="3">Metálico</option>
+                                <option value="4">Solo</option>
                               </select>
                             </div>
                           </div>
@@ -230,7 +229,7 @@
          </div>
       </div>
     </div>
-    <?php include('../assets/php/partials/footer.php') ?>
+    <?php include '../assets/php/partials/footer.php'?>
   </body>
 </html>
 
@@ -248,7 +247,7 @@
 
     fetch('../assets/json/tarifas.json')
       .then(response => response.json())
-      .then(tarifas => {      
+      .then(tarifas => {
         const tarifa = tarifas.find(t => t.id == distribuidora);
         if (tarifa) {
           document.getElementById('valorKwh').textContent = `R$ ${tarifa.tarifa_convencional.toFixed(2)}`;
@@ -270,4 +269,35 @@
     const irradiacaoValor = document.querySelector(`#irradiacao option[value="${irradiacaoId}"]`).getAttribute('data-irradiacao');
     document.getElementById('virradiacao').textContent = `${irradiacaoValor} Wm`;
   }
+</script>
+
+<script>
+  $(document).ready(function () {
+      function calcularPotencia() {
+          let mediaConsumo = parseFloat($('#mediaConsumo').text()) || 0;
+          let quantidadeDias = parseFloat($('#quantidade_dias').val()) || 1;
+          let virradiacao = parseFloat($('#virradiacao').text()) || 1;
+          let eficiencia = parseFloat($('#eficiencia').val()) || 0;
+
+          if (quantidadeDias > 0 && virradiacao > 0 && eficiencia > 0) {
+            console.log(mediaConsumo / quantidadeDias / virradiacao)
+            console.log(eficiencia / 100)
+            let potenciaKit = (mediaConsumo / quantidadeDias / virradiacao) * (eficiencia / 100);
+            console.log(potenciaKit)
+            $('#potencia_kit').val(potenciaKit.toFixed(2));
+          } else {
+            $('#potencia_kit').val(0);
+          }
+      }
+
+      $('#eficiencia').on('input', function () {
+        calcularPotencia();
+      });
+
+      $('#quantidade_dias').on('input', function () {
+          if ($('#eficiencia').val()) {
+            calcularPotencia();
+          }
+      });
+  });
 </script>
