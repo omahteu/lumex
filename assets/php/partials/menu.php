031aa1042da7dee3248a302e98767cd6b56946d3
matheus <matheus.mmn@hotmail.com>
2025-02-08 14:37:07 -0300

    menu e relatórios

--- assets/php/partials/menu.php
+++ assets/php/partials/menu.php
@@ -1,34 +1,28 @@
 <li class="nav-item active">
     <a href="./index.php" class="collapsed">
         <i class="fas fa-home"></i>
-        <p>Dashboard</p>
+        <p>Início</p>
     </a>
 </li>
 <li class="nav-item">
     <a href="./nova_proposta.php">
-        <i class="fas fa-plus"></i>
+        <i class="fa fa-plus" aria-hidden="true"></i>
         <p>Nova Proposta</p>
     </a>
 </li>
 <li class="nav-item">
-    <a href="./propostas.php">
-        <i class="fas fa-file"></i>
-        <p>Propostas</p>
-    </a>
-</li>
-<li class="nav-item">
     <a href="./empresa.php">
-        <i class="fas fa-file"></i>
+        <i class="fas fa-id-card"></i>
         <p>Minha empresa</p>
     </a>
 </li>
 <li class="nav-item">
-    <a data-bs-toggle="collapse" href="#submenu">
+    <a data-bs-toggle="collapse" href="#submenu_cadastros">
         <i class="fas fa-bars"></i>
-        <p>Dados</p>
+        <p>Cadastros</p>
         <span class="caret"></span>
     </a>
-    <div class="collapse" id="submenu">
+    <div class="collapse" id="submenu_cadastros">
         <ul class="nav nav-collapse" id="submenu-list">
             <li>
                 <a href="./clientes.php">
@@ -43,6 +37,37 @@
             <li>
                 <a href="./produtos.php">
                     <span class="sub-item">Produtos</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</li>
+<li class="nav-item">
+    <a data-bs-toggle="collapse" href="#submenu_relatorios">
+        <i class="fa fa-signal"></i>
+        <p>Relatórios</p>
+        <span class="caret"></span>
+    </a>
+    <div class="collapse" id="submenu_relatorios">
+        <ul class="nav nav-collapse" id="submenu-list">
+            <li>
+                <a href="./relatorio_clientes.php">
+                    <span class="sub-item">Clientes</span>
+                </a>
+            </li>
+            <li>
+                <a href="./relatorio_irradiacoes.php">
+                    <span class="sub-item">Irradiações</span>
+                </a>
+            </li>
+            <li>
+                <a href="./relatorio_produtos.php">
+                    <span class="sub-item">Produtos</span>
+                </a>
+            </li>
+            <li>
+                <a href="./produtos.php">
+                    <span class="sub-item">Propostas</span>
                 </a>
             </li>
         </ul>
---
+++ relatorio_clientes.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <?php include('./assets/php/partials/header.php') ?>
+  <body>
+    <div class="wrapper">
+      <!-- Sidebar -->
+      <div class="sidebar" data-background-color="dark">
+        <div class="sidebar-logo">
+          <!-- Logo Header -->
+          <div class="logo-header" data-background-color="dark">
+            <?php include('./assets/php/partials/logo.php') ?>
+            <div class="nav-toggle">
+              <button class="btn btn-toggle toggle-sidebar">
+                <i class="gg-menu-right"></i>
+              </button>
+              <button class="btn btn-toggle sidenav-toggler">
+                <i class="gg-menu-left"></i>
+              </button>
+            </div>
+            <button class="topbar-toggler more">
+              <i class="gg-more-vertical-alt"></i>
+            </button>
+          </div>
+          <!-- End Logo Header -->
+        </div>
+        <div class="sidebar-wrapper scrollbar scrollbar-inner">
+          <div class="sidebar-content">
+            <ul class="nav nav-secondary">
+              <?php include('./assets/php/partials/menu.php'); ?>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <!-- End Sidebar -->
+
+      <div class="main-panel">
+        <div class="main-header">
+          <div class="main-header-logo">
+            <!-- Logo Header -->
+            <div class="logo-header" data-background-color="dark">
+              <a href="../index.php" class="logo">
+                <img
+                  src="#"
+                  alt="navbar brand"
+                  class="navbar-brand"
+                  height="20"
+                />
+              </a>
+              <div class="nav-toggle">
+                <button class="btn btn-toggle toggle-sidebar">
+                  <i class="gg-menu-right"></i>
+                </button>
+                <button class="btn btn-toggle sidenav-toggler">
+                  <i class="gg-menu-left"></i>
+                </button>
+              </div>
+              <button class="topbar-toggler more">
+                <i class="gg-more-vertical-alt"></i>
+              </button>
+            </div>
+            <!-- End Logo Header -->
+          </div>
+          <!-- Navbar Header -->
+
+          <!-- End Navbar -->
+        </div>
+
+        <div class="container">
+          <div class="page-inner">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="card">
+                  <div class="card-header">
+                    <div class="d-flex align-items-center">
+                      <h4 class="card-title">Propostas</h4>
+                      <button class="btn btn-primary btn-round ms-auto" data-bs-toggle="modal" data-bs-target="#addRowModal">
+                          <i class="fa fa-plus"></i>
+                          Nova proposta
+                      </button>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <div class="table-responsive">
+                      <table id="add-row" class="display table table-striped table-hover">
+                        <thead>
+                          <tr>
+                            <th>Name</th>
+                            <th>Position</th>
+                            <th>Office</th>
+                            <th style="width: 10%">Ações</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>Tiger Nixon</td>
+                            <td>System Architect</td>
+                            <td>Edinburgh</td>
+                            <td>
+                              <div class="form-button-action">
+                                <button type="button" data-bs-toggle="tooltip" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task">
+                                  <i class="fa fa-edit"></i>
+                                </button>
+                                <button type="button" data-bs-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Remove">
+                                  <i class="fa fa-times"></i>
+                                </button>
+                              </div>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--   Core JS Files   -->
+    <script src="./assets/js/core/jquery-3.7.1.min.js"></script>
+    <script src="./assets/js/core/popper.min.js"></script>
+    <script src="./assets/js/core/bootstrap.min.js"></script>
+
+    <!-- jQuery Scrollbar -->
+    <script src="./assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+    <!-- Datatables -->
+    
+    <!-- Kaiadmin JS -->
+    <script src="./assets/js/kaiadmin.min.js"></script>
+    <!-- Kaiadmin DEMO methods, don't include it in your project! -->
+    <script src="./assets/js/setting-demo2.js"></script>
+    <script>
+      $(document).ready(function () {
+        $("#basic-datatables").DataTable({});
+
+        $("#multi-filter-select").DataTable({
+          pageLength: 5,
+          initComplete: function () {
+            this.api()
+              .columns()
+              .every(function () {
+                var column = this;
+                var select = $(
+                  '<select class="form-select"><option value=""></option></select>'
+                )
+                  .appendTo($(column.footer()).empty())
+                  .on("change", function () {
+                    var val = $.fn.dataTable.util.escapeRegex($(this).val());
+
+                    column
+                      .search(val ? "^" + val + "$" : "", true, false)
+                      .draw();
+                  });
+
+                column
+                  .data()
+                  .unique()
+                  .sort()
+                  .each(function (d, j) {
+                    select.append(
+                      '<option value="' + d + '">' + d + "</option>"
+                    );
+                  });
+              });
+          },
+        });
+
+        // Add Row
+        $("#add-row").DataTable({
+          pageLength: 5,
+        });
+
+        var action =
+          '<td> <div class="form-button-action"> <button type="button" data-bs-toggle="tooltip" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task"> <i class="fa fa-edit"></i> </button> <button type="button" data-bs-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Remove"> <i class="fa fa-times"></i> </button> </div> </td>';
+
+        $("#addRowButton").click(function () {
+          $("#add-row")
+            .dataTable()
+            .fnAddData([
+              $("#addName").val(),
+              $("#addPosition").val(),
+              $("#addOffice").val(),
+              action,
+            ]);
+          $("#addRowModal").modal("hide");
+        });
+      });
+    </script>
+  </body>
+</html>
